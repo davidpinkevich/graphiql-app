36234c19292c442a86a8bd25a2f4265946cebf8b
davidpinkevich <davidpinkevich95@gmail.com>
2023-05-03 19:06:19 +0300

    feat: add react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
 import ReactDOM from 'react-dom/client';
-import App from './App/App';
+import App from './components/App/App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
