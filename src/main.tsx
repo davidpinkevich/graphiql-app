af1d441230434d7721de42c60d051528047e9f2c
Nikita <nikitakemper555@gmail.com>
2023-05-07 18:49:53 +0300

    feat: add Provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import ReactDOM from 'react-dom/client';
-// import { Provider } from 'react-redux';
-// import store from './redux/store/store';
+import { Provider } from 'react-redux';
 import App from './components/App/App';
+import store from './redux/store';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
-
-// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>
-// );
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
