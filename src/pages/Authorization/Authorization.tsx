af2c1fe3da73a1abdd24ab5ce6e291fc5e0bd247
Anton Vyun <zeta664433@gmail.com>
2023-05-07 12:16:04 +0300

    feat: add template auth page

--- src/pages/Authorization/Authorization.tsx
+++ src/pages/Authorization/Authorization.tsx
@@ -1,5 +1,29 @@
+import { useState } from 'react';
+
+import AuthNav from '../../components/AuthNav/AuthNav';
+import SignInForm from '../../components/SingInForm/SignInForm';
+import SignUpForm from '../../components/SignUpForm/SignUpForm';
+
+import './authorization.scss';
+
 function Authorization() {
-  return <div>Authorization</div>;
+  const [auth, setAuth] = useState<'signin' | 'signup'>('signin');
+
+  const onChangeAuth = (auth: 'signin' | 'signup'): void => {
+    setAuth(auth);
+  };
+
+  const currentForm = auth === 'signin' ? <SignInForm /> : <SignUpForm />;
+  return (
+    <section className="auth">
+      <div className="auth__container">
+        <div className="auth__view">
+          <AuthNav auth={auth} onChangeAuth={onChangeAuth} />
+          {currentForm}
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Authorization;
