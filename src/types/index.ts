eb231bd68617ec36aeba96aacb53ecabacafdd1c
davidpinkevich <davidpinkevich95@gmail.com>
2023-05-08 19:59:15 +0300

    feat: add types for variables

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,6 +2,21 @@ export type TMirrorArea = {
   text: string;
 };
 
+export type TButtonSideBar = {
+  hiiden: boolean;
+  hiddenSide: () => void;
+};
+
+export type TButtonsOptions = {
+  variables: boolean;
+  chooseOption: (arg: boolean) => void;
+};
+
+export type TAreaText = {
+  className: string;
+  mirror: string;
+};
+
 export type TSpanMirror = {
   className: string;
   text: string;
