86fb2a4ee6d0d085d31e4fcd0050ba805b8fa5f9
Anton Vyun <zeta664433@gmail.com>
2023-05-11 20:04:15 +0300

    feat: add template json view

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -3,6 +3,7 @@ import Editor from '../../components/Editor/Editor';
 import React, { ComponentType, Suspense } from 'react';
 import Sidebar from '../../components/Sidebar/Sidebar';
 import CustomLoader from '../../components/Loader/Loader';
+import Response from '../../components/Response/Response';
 
 const Docs = React.lazy(
   () => import('../../components/Docs/Docs') as Promise<{ default: ComponentType }>
@@ -19,6 +20,7 @@ function Main() {
         <Docs />
       </Suspense>
       <Editor />
+      <Response />
     </div>
   );
 }
