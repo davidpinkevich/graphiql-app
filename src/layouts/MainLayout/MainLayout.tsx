69f6abdbe7e9080c50fc404a49779f455a4021a7
Nikita <nikitakemper555@gmail.com>
2023-05-11 19:38:20 +0300

    feat: add styles for docs

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,4 +1,4 @@
-import { NavLink, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
 
