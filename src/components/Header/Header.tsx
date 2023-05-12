6f071126b3206747cb1b68d7b87cc0b25fca76dc
Anton Vyun <zeta664433@gmail.com>
2023-05-12 22:08:20 +0300

    feat: add navigation to page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,15 @@
-import './Header.scss';
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+import { AppDispatch } from '../../redux/store';
+import { onAuthChange } from '../../redux/slices/auth';
+
+import './Header.scss';
 
 function Header() {
+  const dispatch = useDispatch<AppDispatch>();
+
   const [isScrolled, setIsScrolled] = useState(false);
 
   useEffect(() => {
@@ -16,11 +24,29 @@ function Header() {
     };
   });
 
+  const navigate = useNavigate();
+
   return (
     <header className={`header ${isScrolled ? 'scrolled' : ''}`}>
       <div className="header__actions">
-        <button className="button signup-btn">Sign Up</button>
-        <button className="button signin-btn">Sign In</button>
+        <button
+          className="button signup-btn"
+          onClick={() => {
+            dispatch(onAuthChange('signup'));
+            navigate('/authorization');
+          }}
+        >
+          Sign Up
+        </button>
+        <button
+          className="button signin-btn"
+          onClick={() => {
+            dispatch(onAuthChange('signin'));
+            navigate('/authorization');
+          }}
+        >
+          Sign In
+        </button>
         <button className="button signout-btn">Sign Out</button>
       </div>
     </header>
